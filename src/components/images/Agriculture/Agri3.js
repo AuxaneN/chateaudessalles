258894c1e47515a4b7ee2b13a5c21665c4e7f83c
Auxane Nitharum <auxane.nith@protonmail.com>
2020-09-23 12:05:43 +0200

    Swapped olive tree image

--- src/components/images/Agriculture/Agri3.js
+++ src/components/images/Agriculture/Agri3.js
@@ -5,9 +5,11 @@ import Img from "gatsby-image"
 const Agri3 = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "Agriculture/agriculture_3.jpg" }) {
+      placeholderImage: file(
+        relativePath: { eq: "Agriculture/Agriculture_oliveraie.jpeg" }
+      ) {
         childImageSharp {
-          fluid(quality: 90, maxWidth:700) {
+          fluid(quality: 90, maxWidth: 700) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
@@ -15,7 +17,12 @@ const Agri3 = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} alt="Un chemin encadré d'arbres mène a travers l'oliverae"/>
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      alt="Un chemin encadré d'arbres mène a travers l'oliverae"
+    />
+  )
 }
 
 export default Agri3
